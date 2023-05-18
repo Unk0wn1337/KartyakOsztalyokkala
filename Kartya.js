1c1f14979a43fb032a101d1e331fa92d58809da1
Unk0wn1337 <kelogames@gmail.com>
2023-05-18 15:15:25 +0200

    shuffle

--- Kartya.js
+++ Kartya.js
@@ -11,22 +11,43 @@ class Kartya{
            this.#allapot = false;
            szuloELEM.append(
                `<div class "kartyak">
-                   <img src="${this.#fajlnev}" alt="fajl">
+                   <img src="./kepek/hatter.jpg" alt="fajl">
                </div>`
                
                );
+            this.#divElem = szuloELEM.children("div:last-child");
+            this.#imgElem = this.#divElem.children("img");
+            this.#divElem.on("click", () => {
+                this.#kattintasTrigger();
+                this.#setLap();
+                console.log(); 
+            })
+
    
        }
        setAllapot(){
+        this.#allapot = !this.#allapot;
+        this.#setLap();
    
        }
        getFajlnev(){
+        return this.#fajlnev;
            
        }
        #setLap(){
-   
-       }
-       #kattintasTrigger(){
+        if (this.#allapot){
+            this.#imgElem.attr("src",this.#fajlnev);
+        } else {
+            this.#imgElem.attr("src", "kepek/hatter.jpg");
+        }
+
+        }
+        
+
+    #kattintasTrigger(){
+           const fordit = new CustomEvent("kepreKattintas", {detail: this});
+           window.dispatchEvent(fordit);
+            
    
        }
    
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+import { kartyaLista} from "./kepek.js";
 import JatekTer from "./JatekTer.js";
 $(function() {
-    const Jatek = new JatekTer();
+    const Jatek = new JatekTer(kartyaLista);
+    
     
 
 })
