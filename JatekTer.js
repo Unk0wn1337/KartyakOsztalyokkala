d9dbb92dc5a00b41628b3cca767029b9f27fa64a
Unk0wn1337 <kelogames@gmail.com>
2023-05-18 15:15:08 +0200

    shuffle,kartya

--- JatekTer.js
+++ JatekTer.js
@@ -1,14 +1,17 @@
 import Kartya from "./Kartya.js";
-import { kartyaLista } from "./kepek.js";
+
 class JatekTer{
     #kartyaLista = [];
     #kivalasztottKartyaLista = [];
-
-    constructor(){
+    
+    constructor(kartyaLista){
+        console.log(kartyaLista);
         const szuloELEM = $("#jatekTerem");
         this.#kartyaLista = kartyaLista;
-        for (let index = 0; index < kartyaLista.length; index++) {
-            const kartya = new Kartya(szuloELEM,kartyaLista[index]);
+        this.#kever();
+        for (let index = 0; index < this.#kartyaLista.length; index++) {
+            const kartya = new Kartya(szuloELEM,this.#kartyaLista[index]);
+            
         }
             
 
@@ -17,13 +20,20 @@ class JatekTer{
         
     }
     #kever(){
+        for (let i = this.#kartyaLista.length-1; i >= 0; i--) {
+            let index = parseInt(Math.random()*(i+1));
+            let tempt = this.#kartyaLista[index];
+            this.#kartyaLista[index] = this.#kartyaLista[i];
+            this.#kartyaLista[i] = tempt;
+        }
 
     }
     #ellenorzes(){
+        
 
     }
     #TriggerBlocked(){
-
+        
     }
     
     #TriggerUnBlocked(){
